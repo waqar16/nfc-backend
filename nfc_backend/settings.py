5e7edea40cd89800ede65554b07d282e86ebc3fd
waqar16 <67152603+waqar16@users.noreply.github.com>
2024-08-28 22:03:42 +0500

    .

--- nfc_backend/settings.py
+++ nfc_backend/settings.py
@@ -202,8 +202,8 @@ DJOSER = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
